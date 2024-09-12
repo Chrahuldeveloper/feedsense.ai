4f9b2bab61f2d31ab11071edb7dcd98505bf62df
Rahul <chrahulofficial@gmail.com>
2024-09-12 19:22:35 +0530

    done

--- src/app/dashboard/reviewstable/page.tsx
+++ src/app/dashboard/reviewstable/page.tsx
@@ -54,30 +54,15 @@ const ReviewsTable = () => {
     },
   ]);
 
-  interface Review {
-    taskName: string;
-    taskStatus: "Pending" | "In Progress" | "Completed";
-  }
-
-  const handleStatusChange = (
-    index: number,
-    newStatus: Review["taskStatus"]
-  ) => {
-    const updatedReviews = [...reviews];
-    updatedReviews[index].taskStatus = newStatus;
-    setReviews(updatedReviews);
-  };
-
   return (
-    <div className="bg-[#000000] w-full  flex gap-5 overflow-x-clip ">
+    <div className="bg-[#000000] w-full h-screen flex gap-5 overflow-x-clip ">
       <SideBar />
-      <div className="w-[88vw] md:w-[80vw] mx-auto my-12 md:ml-48">
+      <div className="w-[89vw] md:w-[60vw] mx-auto my-12 md:ml-80">
         <table className="text-white w-full table-auto py-6 border-[1px] border-neutral-900">
           <thead className="bg-[#0f0d15]">
             <tr className="text-center text-xs">
               <th className="py-2 px-4 text-xs">S.No</th>
               <th className="py-2 px-4 text-xs">Task Name</th>
-              <th className="py-2 px-4 text-xs">Task Status</th>
             </tr>
           </thead>
           <tbody>
@@ -93,22 +78,6 @@ const ReviewsTable = () => {
                 </td>
                 <td className="py-2 px-4 text-[11px] md:text-sm ">
                   {review.taskName}
-                </td>
-                <td className="py-2 px-4 text-[11px] md:text-sm ">
-                  <select
-                    onChange={(e) =>
-                      handleStatusChange(
-                        index,
-                        e.target.value as Review["taskStatus"]
-                      )
-                    }
-                    value={review.taskStatus}
-                    className="bg-[#0f0d15] text-white  rounded-lg px-3 py-2 outline-none cursor-pointer"
-                  >
-                    <option value="Pending">Pending</option>
-                    <option value="In Progress">In Progress</option>
-                    <option value="Completed">Completed</option>
-                  </select>
                 </td>
               </tr>
             ))}
