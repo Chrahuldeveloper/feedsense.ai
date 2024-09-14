b67d438d8808b1712b680b1b80c4fe8cca7e7ad5
Rahul <chrahulofficial@gmail.com>
2024-09-14 21:18:31 +0530

    done

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,6 @@
 import React, { useState } from "react";
 import Table from "@/components/Table";
 import SideBar from "@/components/SideBar";
-import Notifications from "@/components/Notifications";
 
 const Page = () => {
   const [section, setsection] = useState();
@@ -10,13 +9,7 @@ const Page = () => {
   return (
     <div className="bg-[#000000] w-full  flex overflow-x-clip">
       <SideBar setsection={setsection} />
-      {section === "dashboard" ? (
-        <Table />
-      ) : section === "notifications" ? (
-        <Notifications />
-      ) : (
-        <Table />
-      )}
+      {section === "dashboard" ? <Table /> : <Table />}
     </div>
   );
 };
