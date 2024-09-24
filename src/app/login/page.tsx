da0c17d0f184c9ba96b30ade9a9dfcf6585e74d0
Rahul <chrahulofficial@gmail.com>
2024-09-24 20:39:46 +0530

    dn

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,27 @@
-import React from "react";
-
+"use client";
+import React, { useState } from "react";
+import AppWriteService from "../../appwrite/config";
 const page = () => {
+  const auth = new AppWriteService();
+
+  const [data, setdata] = useState({
+    email: "",
+    password: "",
+    name: "",
+  });
+
+  const handleSubmit = () => {
+    auth.creatUserAccount({
+      email: data.email,
+      password: data.password,
+      name: data.name,
+    });
+  };
+
+  const handleChange = (e: any) => {
+    setdata({ ...data, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="bg-[rgb(0,0,0)] w-full overflow-y-clip h-screen md:h-auto ">
       <div className="md:flex gap-8 items-center pt-20 md:pt-0">
@@ -11,7 +32,6 @@ const page = () => {
             className="h-[100vh] hidden md:block w-[50vw] object-cover brightness-75"
           />
         </div>
-        {/* border-[1px] border-red-500 */}
         <div className="text-slate-300 bg-[#131b2376] p-7 rounded-lg w-[90vw] md:w-[30vw] mx-auto md:my-0 ">
           <div className="space-y-3.5">
             <h1 className="text-2xl font-bold ">Welcome back</h1>
@@ -22,28 +42,37 @@ const page = () => {
             <h1 className="font-semibold ">Name*</h1>
             <input
               type="text"
+              value={data.name}
+              onChange={handleChange}
               autoComplete="false"
-              className="bg-[#0d0d13] border-[1px] border-neutral-900 px-2 py-2 outline-none md:w-[25vw] w-[85vw] rounded-lg"
+              className="bg-[#0d0d13] border-[1px] border-neutral-900 px-2 py-2 outline-none md:w-[25vw] w-[80vw] rounded-lg"
             />
           </div>
           <div className="space-y-2 mt-4">
             <h1 className="font-semibold ">Email*</h1>
             <input
               type="text"
+              value={data.email}
+              onChange={handleChange}
               autoComplete="false"
-              className=" bg-[#0d0d13] border-[1px] border-neutral-900 px-2 py-2 outline-none md:w-[25vw] w-[85vw] rounded-lg"
+              className=" bg-[#0d0d13] border-[1px] border-neutral-900 px-2 py-2 outline-none md:w-[25vw] w-[80vw] rounded-lg"
             />
           </div>
           <div className="space-y-2 mt-4">
             <h1 className="font-semibold ">Password*</h1>
             <input
               type="password"
+              value={data.password}
+              onChange={handleChange}
               autoComplete="false"
-              className="bg-[#0d0d13] border-[1px] border-neutral-900 px-2 py-2 outline-none md:w-[25vw] w-[85vw] rounded-lg"
+              className="bg-[#0d0d13] border-[1px] border-neutral-900 px-2 py-2 outline-none md:w-[25vw] w-[80vw] rounded-lg"
             />
           </div>
 
-          <button className="bg-blue-500 mt-6  text-white  py-2 md:w-[25vw]  w-[85vw]  font-semibold rounded-lg hover:bg-blue-600 ease-in-out duration-500">
+          <button
+            onClick={handleSubmit}
+            className="bg-blue-500 mt-6  text-white  py-2 md:w-[25vw]  w-[80vw]  font-semibold rounded-lg hover:bg-blue-600 ease-in-out duration-500"
+          >
             Login
           </button>
           <div className="text-center my-3.5 text-slate-300">
