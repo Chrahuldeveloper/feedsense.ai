d0d5016e398129b88faaec92879d4d000e8ccef1
Rahul <chrahulofficial@gmail.com>
2024-10-28 23:11:35 +0530

    done

---
+++ src/ai/AiService.ts
@@ -0,0 +1,5 @@
+export default class AiService {
+  async analyseFeedback(data: string): Promise<string> {
+    return "";
+  }
+}
---
+++ src/ai/index.js
@@ -0,0 +1,15 @@
+export default {
+  async fetch(request, env) {
+    const tasks = [];
+
+    const feedback = await request.json();
+
+    const taskInput = {
+      prompt: `Convert the following feedback into a short actionable task that is between 15 and 20 characters long: ${feedback.message}`,
+    };
+
+    let response = await env.AI.run("@cf/meta/llama-3-8b-instruct", taskInput);
+    tasks.push({ inputs: taskInput, response });
+    return Response.json(tasks);
+  },
+};
