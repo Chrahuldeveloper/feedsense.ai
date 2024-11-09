9716e0f254ff22467243589c2bfcbc735d752277
Rahul <chrahulofficial@gmail.com>
2024-11-10 00:03:22 +0530

    done

--- src/app/plans/page.tsx
+++ src/app/plans/page.tsx
@@ -38,38 +38,43 @@ export default function PricingPage() {
 
       <div className="relative z-10">
         <Navbar />
-        <div className="flex items-center justify-center min-h-screen relative -mt-28">
-          <div className="absolute inset-2 flex items-center justify-center"></div>
-          <div className="p-10 rounded-lg text-center relative z-10">
+        <div className="flex items-center justify-start md:pl-64 min-h-screen relative -mt-48">
+          <div className="absolute inset-2 flex "></div>
+          <div className="p-10 rounded-lg  relative z-10 space-y-6">
             <h1 className="text-4xl md:text-5xl font-semibold text-white max-w-2xl">
-              Simple, transparent pricing with no surprises
+              Choose your Plan
             </h1>
+            <p className="text-white max-w-xs">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
           </div>
         </div>
 
-        <div className="flex flex-col md:flex-row gap-8 px-6 md:px-10 pb-14 justify-center -mt-36">
+        <div className="flex flex-col md:flex-row gap-8 px-6 md:px-10 pb-14 justify-center -mt-48">
           {Plans.map((plan, idx) => (
             <div
               key={idx}
               className="flex flex-col rounded-lg border-[1px] bg-[#121212] border-[#282e32] shadow-lg transform transition-transform duration-300 hover:scale-105 w-full max-w-md cursor-pointer"
             >
-              <div className="bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 p-6 space-y-4 rounded-t-lg text-center">
+              <div className="bg-gradient-to-r from-blue-400 via-blue-600 to-blue-700 p-6 space-y-4 rounded-t-lg text-center">
                 <h1 className="text-white text-2xl font-bold">{plan.name}</h1>
-                <p className="text-lg font-semibold text-slate-200">{plan.price}</p>
+                <p className="text-lg font-semibold text-slate-200">
+                  {plan.price}
+                </p>
               </div>
 
               <ul className="space-y-3 text-slate-300 p-6 flex-grow">
                 {plan.features.map((feature, index) => (
-                  <li key={index} className="text-sm flex items-start gap-2">
+                  <li key={index} className=" flex items-start gap-2">
                     <TiTickOutline size={20} className="text-green-500" />
-                    <span>{feature}</span>
+                    <span className="font-semibold">{feature}</span>
                   </li>
                 ))}
               </ul>
 
               <div className="p-6 mt-auto">
                 <Link href={`/subscription/${plan.name}`}>
-                  <button className="w-full bg-gradient-to-r from-blue-400 via-blue-600 to-blue-700 mt-4 text-white py-3 font-semibold rounded-full shadow-md hover:shadow-lg transition-shadow duration-200">
+                  <button className="w-full bg-gradient-to-r from-blue-400 via-blue-600 to-blue-700 mt-4 text-white py-3 font-semibold rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200">
                     Get Plan
                   </button>
                 </Link>
