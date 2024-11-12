ed91f8575b0621c2fab4d68efce266f44bf06d0d
Rahul <chrahulofficial@gmail.com>
2024-11-12 17:28:37 +0530

    dn

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -3,21 +3,25 @@ import Razorpay from "razorpay";
 
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID!,
-  key_secret: process.env.RAZORPAY_KEY_SECRET!,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
 export async function POST(req: NextRequest) {
   try {
     const { amount } = await req.json();
-    const order = razorpay.orders.create({
+
+    const order = await razorpay.orders.create({
       amount: amount,
       currency: "INR",
       receipt: "receipt_" + Math.random().toString(36).substring(7),
     });
 
-    return NextResponse.json({ orderId: (await order).id }, { status: 200 });
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json({ error: error }, { status: 500 });
+    return NextResponse.json({ orderId: order.id }, { status: 200 });
+  } catch (error:any) {
+    console.log("Error creating Razorpay order:", error);
+    return NextResponse.json(
+      { error: error.message || "An error occurred" },
+      { status: 500 }
+    );
   }
 }
