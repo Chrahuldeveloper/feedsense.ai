cb16992802cd04f227a4b8af39ba9d890c6810ce
Rahul <chrahulofficial@gmail.com>
2025-01-04 22:49:49 +0530

    done

--- src/app/dashboard/tasks/page.tsx
+++ src/app/dashboard/tasks/page.tsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 import { BiConfused } from "react-icons/bi";
 import { RxCross2 } from "react-icons/rx";
 import Image from "next/image";
-import { Bar, Line } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -49,20 +49,40 @@ const Page = () => {
   const getName = searchParams!.get("name")!;
   const getPlan = searchParams!.get("Plan")!;
 
+  const [HappyCount, setHappyCount] = useState<number>(0);
+  const [NeutralCount, setNeutralCount] = useState<number>(0);
+  const [SadCount, setSadCount] = useState<number>(0);
+
   useEffect(() => {
     const data: Feedback[] = JSON.parse(getdata);
     console.log(data);
     setWebsites(data);
+
+    const happy = data.filter(
+      (item) => item.emotion.toLowerCase() === "happy"
+    ).length;
+    const neutral = data.filter(
+      (item) => item.emotion.toLowerCase() === "neutral"
+    ).length;
+    const sad = data.filter(
+      (item) => item.emotion.toLowerCase() === "sad"
+    ).length;
+
+    setHappyCount(happy);
+    setNeutralCount(neutral);
+    setSadCount(sad);
   }, [getdata]);
 
   const [toggle, setToggle] = useState(false);
+
+  console.log(HappyCount, NeutralCount, SadCount);
 
   const analytics = {
     labels: ["Happy", "Neutral", "Sad"],
     datasets: [
       {
         label: "Dashboard Data",
-        data: [10, 20, 30],
+        data: [HappyCount, NeutralCount, SadCount],
         borderColor: "#2967ec",
         pointBackgroundColor: "#2967ec",
         pointBorderColor: "#2967ec",
@@ -85,11 +105,11 @@ const Page = () => {
         />
       </nav>
 
-      <div className="bg-[black] w-full  flex overflow-x-clip">
+      <div className="bg-[#111115] w-full flex overflow-x-clip">
         <SideBar page="Home" />
         <div className="md:w-[100vw] mx-auto md:ml-44 space-y-16 rounded-xl ">
           <div className="overflow-x-auto rounded-xl my-12">
-            <div className="flex mx-auto items-center justify-between w-[90vw] md:w-[60vw] bg-[#07070b] px-4 py-2 border-[1px] border-[#0e1012]">
+            <div className="flex mx-auto items-center justify-between w-[90vw] md:w-[60vw] bg-[#0e0f12] px-4 py-2 border-[1px] border-[#0e1012]">
               <div className="flex items-center gap-5">
                 <Image
                   src={getImage}
@@ -107,7 +127,7 @@ const Page = () => {
               </Link>
             </div>
             <table className=" border-[1px] border-[#15171b] md:mx-auto w-[98vw] md:w-[60vw] mt-7 divide-y divide-[#15171b]  overflow-hidden ">
-              <thead className="bg-[#04050a]  cursor-pointer border-[1px] border-[#15171b]">
+              <thead className="bg-[#0e0f12]  cursor-pointer border-[1px] border-[#15171b]">
                 <tr>
                   <th className="py-3 px-5 md:px-9 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
                     Email
@@ -123,7 +143,7 @@ const Page = () => {
                   </th>
                 </tr>
               </thead>
-              <tbody className="bg-[#04050a] border-[1px] border-[#15171b]">
+              <tbody className="bg-[#0e0f12] border-[1px] border-[#15171b]">
                 {websites.length > 0 ? (
                   websites.map((site, idx) => (
                     <tr
@@ -162,12 +182,13 @@ const Page = () => {
               </tbody>
             </table>
           </div>
-
-          <div className="w-[90vw] md:w-[50vw] mx-auto mb-5">
-            <h2 className="text-gray-300 text-lg font-semibold mb-4">
-              Bar Graph
-            </h2>
-            <Line data={analytics} />
+          <div>
+            <div className="w-[90vw] md:w-[60vw] mx-auto mb-5 bg-[#0e0f12] p-4">
+              <h2 className="text-gray-300 text-lg font-semibold mb-4">
+                Bar Graph
+              </h2>
+              <Line data={analytics} />
+            </div>
           </div>
         </div>
       </div>
