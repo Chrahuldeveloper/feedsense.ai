b182ba4c545921f70cf281acd72ad1b89e6f0c2c
Rahul <chrahulofficial@gmail.com>
2025-01-17 17:41:41 +0530

    done

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -73,6 +73,8 @@ const Form = () => {
     }
   };
 
+  console.log(feedbackStatus);
+
   const emotions: Emotion[] = [
     { title: "Happy", emoji: <IoHappyOutline size={32} color="#c1d0d5" /> },
     { title: "Neutral", emoji: <IoSadOutline size={32} color="#c1d0d5" /> },
@@ -81,20 +83,24 @@ const Form = () => {
 
   return (
     <div className="flex justify-center p-5 pt-5">
-      <div className="rounded-lg shadow-lg w-full max-w-md sm:max-w-lg lg:max-w-xl border-[1px] bg-[#121212] border-[#282e32]">
+      <div className="rounded-lg shadow-lg w-full max-w-lg sm:max-w-lg lg:max-w-xl border-[1px] bg-[#121212] border-[#282e32]">
         {loading ? (
           <>
             <div className="fixed inset-0 z-50 flex items-center justify-center h-full bg-black bg-opacity-70 blur-xs">
               <div className="max-w-xs mx-auto -mt-10">
-                <LottiePlayer loop animationData={FeedbackLoader} play className="w-36 lg:w-52"/>
+                <LottiePlayer
+                  loop
+                  animationData={FeedbackLoader}
+                  play
+                  className="w-36 lg:w-52"
+                />
               </div>
-              <p className="text-white  text-center -mt-28">{feedbackStatus}</p>
             </div>
           </>
         ) : (
           <div className="">
             <div className="bg-[#1e1e1e] p-6 rounded-t-lg border-b-[1px] border-stone-800 space-y-2">
-              <h1 className="text-2xl font-bold text-white">
+              <h1 className="text-xl font-bold text-white">
                 Your Opinion Matters
               </h1>
               <p className="text-white text-xs font-semibold">
@@ -149,7 +155,7 @@ const Form = () => {
                       setFeedback({ ...feedback, emotion: emotion.title });
                     }}
                     className={`cursor-pointer p-2 rounded-full ${
-                      selectedEmotion === idx ? "bg-blue-600" : ""
+                      selectedEmotion === idx ? "bg-[#282e32]" : ""
                     }`}
                   >
                     {emotion.emoji}
@@ -161,7 +167,7 @@ const Form = () => {
             <div className="mt-4 p-5">
               <button
                 onClick={saveFeedBack}
-                className="text-black font-semibold px-4 py-2 rounded-lg w-full bg-slate-50"
+                className="text-black font-semibold px-4 py-2 rounded-lg w-full bg-slate-50 text-sm"
               >
                 Share Insights
               </button>
