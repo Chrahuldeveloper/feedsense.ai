794872d8bf46ccd3fdb9e86ea4a286820566e058
Rahul <chrahulofficial@gmail.com>
2025-02-14 01:23:06 +0530

    done

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -163,8 +163,8 @@ const Table = () => {
               </p>
             </div>
           ) : (
-            <div className="overflow-x-auto pt-5">
-              <table className="min-w-full mx-auto divide-y overflow-x-scroll rounded-xl divide-[#15171b] border-[1px] border-[#15171b]">
+            <div className="overflow-x-scroll pt-5">
+              <table className=" w-[88vw] md:w-full mx-auto divide-y overflow-x-scroll rounded-xl divide-[#15171b] border-[1px] border-[#15171b]">
                 <thead className="bg-[#111115]">
                   <tr className="cursor-pointer">
                     <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
