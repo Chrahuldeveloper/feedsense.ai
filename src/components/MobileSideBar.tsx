16e5b59711738af94b36661b49697c905491faeb
Rahul <chrahulofficial@gmail.com>
2025-02-14 00:51:03 +0530

    done

--- src/components/MobileSideBar.tsx
+++ src/components/MobileSideBar.tsx
@@ -13,7 +13,7 @@ interface MobileSideBarProps {
 
 const MobileSideBar: React.FC<MobileSideBarProps> = ({ setToggle }) => {
   return (
-    <aside className="bg-[#111115] py-6 w-[75vw] h-full fixed top-0 left-0 md:hidden space-y-10 px-10 border-r-[1px] border-[#272b2f]">
+    <aside className="bg-[#111115] py-6 w-[70vw] h-full fixed top-0 left-0 md:hidden space-y-10 px-10 border-r-[1px] border-[#272b2f]">
       <div className="flex justify-end cursor-pointer">
         <RxCross2
           size={23}
@@ -27,32 +27,32 @@ const MobileSideBar: React.FC<MobileSideBarProps> = ({ setToggle }) => {
       <Link href="/dashboard">
         <div className="flex items-center space-x-2.5 w-32 justify-between cursor-pointer my-10">
           <MdDashboard size={22} color="white" cursor={"pointer"} />
-          <h1 className="text-slate-300 font-semibold">DashBoard</h1>
+          <h1 className="text-slate-300 font-semibold text-sm ">DashBoard</h1>
         </div>
       </Link>
       <Link href="/dashboard/integrate">
         <div className="flex items-center space-x-2.5 w-32 justify-between cursor-pointer my-10">
           <CgWebsite size={22} color="white" cursor={"pointer"} />
-          <h1 className="text-slate-300 font-semibold">Integrate</h1>
+          <h1 className="text-slate-300 font-semibold text-sm ">Integrate</h1>
         </div>
       </Link>
       <Link href="/dashboard/contact">
         <div className="flex items-center space-x-2.5 w-32 justify-between cursor-pointer my-10">
           <IoIosCall size={22} color="white" cursor={"pointer"} />
-          <h1 className="text-slate-300 font-semibold">Contact Us</h1>
+          <h1 className="text-slate-300 font-semibold text-sm ">Contact Us</h1>
         </div>
       </Link>
 
       <Link href="/plans">
         <div className="flex items-center space-x-2.5 w-32 justify-between cursor-pointer">
           <MdOutlineAttachMoney size={22} color="white" cursor={"pointer"} />
-          <h1 className="text-slate-300 font-semibold">Subcription</h1>
+          <h1 className="text-slate-300 font-semibold text-sm ">Subcription</h1>
         </div>
       </Link>
 
       <div className="flex items-center space-x-2.5 w-32 justify-between cursor-pointer">
         <IoLogOut size={22} color="white" cursor={"pointer"} />
-        <h1 className="text-slate-300 font-semibold">LogOut</h1>
+        <h1 className="text-slate-300 font-semibold text-sm ">LogOut</h1>
       </div>
     </aside>
   );
