b2fc218ad0f51c349dd745a8d0a1d3f0d730be6e
Rahul <chrahulofficial@gmail.com>
2025-02-14 11:35:44 +0530

    done

--- src/app/dashboard/tasks/page.tsx
+++ src/app/dashboard/tasks/page.tsx
@@ -125,8 +125,8 @@ const Page = () => {
         <div className="md:w-[100vw] mx-auto md:ml-44 space-y-16 rounded-xl">
           {/* User Info */}
           <div className="overflow-x-auto rounded-xl my-12">
-            <div className="flex mx-auto items-center justify-between w-[90vw] md:w-[60vw] bg-[#111115] px-4 py-2 border-[1px] border-[#0e1012]">
-              <div className="flex items-center gap-5">
+            <div className="flex mx-auto items-center justify-between w-[100vw] md:w-[60vw] bg-[#111115] px-8 py-2 border-[1px] border-[#0e1012]">
+              <div className="flex items-center gap-4">
                 <Image
                   src={getImage}
                   alt={getName}
@@ -134,7 +134,7 @@ const Page = () => {
                   width={48}
                   height={48}
                 />
-                <h1 className="text-xl text-gray-300 font-semibold">
+                <h1 className="text-sm md:text-xl text-gray-300 font-semibold">
                   {getName}
                 </h1>
               </div>
@@ -193,7 +193,7 @@ const Page = () => {
                           {idx + 1}
                         </td>
                         <td
-                          className={`py-4 px-10 lg:pl-48 text-left text-[11px] text-gray-300 ${
+                          className={`py-4 px-10 lg:pl-48 text-left text-[9px] md:text-xs text-gray-300 ${
                             getPlan === "Basic" ? "blur-md" : ""
                           }`}
                         >
@@ -277,7 +277,7 @@ const Page = () => {
             </div>
 
             {/* Feedback Table */}
-            <table className="border-[1px] border-[#15171b] md:mx-auto w-[75vw] md:w-[60vw] mx-auto mt-7 divide-y divide-[#15171b] ">
+            <table className="border-[1px] border-[#15171b] md:mx-auto w-[100vw] md:w-[60vw] mx-auto mt-7 divide-y divide-[#15171b] ">
               <thead className="bg-[#111115] border-[1px] border-[#15171b]">
                 <tr>
                   <th className="py-3 px-3 md:px-9 text-left text-[10px] font-medium text-gray-400 uppercase">
