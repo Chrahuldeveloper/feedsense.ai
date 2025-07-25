13bcc72ae3550935f4b0bf8dad086dd46c0536e6
Rahul <chrahulofficial@gmail.com>
2025-07-26 02:32:50 +0530

    done

--- src/app/dashboard/tasks/page.tsx
+++ src/app/dashboard/tasks/page.tsx
@@ -90,9 +90,9 @@ const Page = () => {
           (item) => item.emotion.toLowerCase() === "sad"
         ).length;
 
-        setHappyCount(happy);
-        setNeutralCount(neutral);
-        setSadCount(sad);
+        setHappyCount(10);
+        setNeutralCount(10);
+        setSadCount(10);
       } catch (e) {
         console.error("Error parsing feedback:", e);
       }
@@ -105,7 +105,7 @@ const Page = () => {
       {
         label: "Feedback Summary",
         data: [HappyCount, NeutralCount, SadCount],
-        backgroundColor: ["#00a3ff", "#9f7aea", "#fc8181"],
+        backgroundColor: ["white", "#00bfff", "#3c4a5a"],
         hoverOffset: 4,
       },
     ],
@@ -137,193 +137,165 @@ const Page = () => {
         <SideBar page="Home" />
         <div className="md:w-[100vw] mx-auto md:ml-44 space-y-16 rounded-xl">
           <div className="overflow-x-auto rounded-xl my-12">
-            <div className="flex md:mx-auto items-center justify-center rounded-lg w-[100vw] md:w-[73vw] px-8 py-2 border-[1px] border-[#0e1012]">
-              <div className="flex flex-col items-center gap-4">
+            <div className="flex md:mx-auto  rounded-lg w-[100vw] md:w-[73vw] px-8 py-2 border-[1px] border-[#0e1012]">
+              <div className="flex flex-col  gap-4">
                 <h1 className="text-4xl font-bold bg-gradient-to-r from-[#68a8fd] to-cyan-500 bg-clip-text text-transparent">
                   AI-Powered Insights
                 </h1>
-                <p className="text-[#95a4ab]">
+                <p className="text-[#95a4ab] text-sm">
                   Unlock the power of artificial intelligence to understand your
                   users better and make data-driven decisions
                 </p>
               </div>
             </div>
 
-            {HappyCount > 0 && NeutralCount > 0 && SadCount > 0 ? (
-              <Doughnut data={analytics} className="w-96 mx-auto p-10" />
-            ) : null}
+           
 
-            <div className="flex justify-center items-center flex-col md:flex-row gap-10 my-6">
-              {[
-                {
-                  title: "Happy",
-                  count: HappyCount,
-                  icon: (
-                    <IoMdHappy
-                      size={33}
-                      color="#00a3ff"
-                      className="w-12 p-2 h-12 rounded-full bg-[#13293c]"
-                    />
-                  ),
-                },
-                {
-                  title: "Neutral",
-                  count: NeutralCount,
-                  icon: (
-                    <MdOutlineSentimentNeutral
-                      size={33}
-                      color="#00a3ff"
-                      className="w-12 p-2 h-12 rounded-full bg-[#13293c]"
-                    />
-                  ),
-                },
-                {
-                  title: "Sad",
-                  count: SadCount,
-                  icon: (
-                    <IoMdSad
-                      size={33}
-                      color="#00a3ff"
-                      className="w-12 p-2 h-12 rounded-full bg-[#13293c]"
-                    />
-                  ),
-                },
-              ].map(({ title, count, icon }, idx) => (
-                <div
-                  key={title}
-                  className="bg-[#161617] py-5 px-5 rounded-xl w-[60vw] lg:w-[18vw] flex justify-evenly gap-5 cursor-pointer border-[1px] border-[#2c2c2d] hover:scale-105 ease-in-out duration-500"
-                >
-                  <div className="space-y-3">
-                    <h1 className="text-white text-xl">{title} Count</h1>
-                    <p className="text-lg text-gray-300">{count}</p>
+            <div className="flex flex-col lg:flex-row justify-center items-center lg:ml-28">
+              <div className="w-[86vw] md:w-[70vw] lg:w-[36vw] mx-auto p-5 rounded-xl bg-[#161617] h-[50vh]">
+                <div className="flex justify-between">
+                  <div className="flex items-center space-x-3">
+                    <LuBrain size={25} color="#00a3ff" />
+                    <h1 className="font-semibold text-white">AI Insights</h1>
                   </div>
-                  {icon}
+                  <div className="text-sm text-white">AI analyzing...</div>
                 </div>
-              ))}
-            </div>
 
-            <div className="w-[86vw] md:w-[70vw] lg:w-[58vw] mx-auto p-5 rounded-xl bg-[#161617] h-[50vh]">
-              <div className="flex justify-between">
-                <div className="flex items-center space-x-3">
-                  <LuBrain size={25} color="#00a3ff" />
-                  <h1 className="font-semibold text-white">AI Insights</h1>
-                </div>
-                <div className="text-sm text-white">AI analyzing...</div>
-              </div>
-
-              <div className="flex justify-center flex-col gap-3 mt-4">
-                {feedbackData.length > 0 ? (
-                  feedbackData.map((fb, idx) =>
-                    fb?.parsedFeedback?.response ? (
-                      <div
-                        key={idx}
-                        className={`bg-[#2c2016] text-[#d1d1d1] flex items-center rounded-lg p-5 gap-4 border-l-4 border-orange-500 w-full mb-4 ${
-                          getPlan === "Basic"
-                            ? "cursor-not-allowed"
-                            : "cursor-pointer"
-                        }`}
-                      >
-                        <div className="bg-[#244445] p-3 rounded-md text-cyan-400 text-xl">
-                          💡
-                        </div>
-                        <div className="flex-1">
-                          <p
-                            className={`${
-                              getPlan === "Basic" ? "blur-sm" : ""
-                            }`}
-                          >
-                            {fb.parsedFeedback.response.replace(/"/g, "")}
-                          </p>
-                        </div>
-                        <div className="flex items-center space-x-3">
-                          <RxCross2
-                            size={40}
-                            color="red"
-                            className="hover:bg-red-900 p-2 rounded-full cursor-pointer"
-                            onClick={() => {
-                              if (getPlan !== "Basic") {
-                                setFeedbackData((prev) =>
-                                  prev.filter((_, i) => i !== idx)
-                                );
-                              }
-                            }}
-                          />
-                          <FiCheck
-                            size={40}
-                            color="green"
-                            className={`hover:bg-green-900 p-2 rounded-full cursor-pointer ${
-                              getPlan === "Basic"
-                                ? "opacity-50 cursor-not-allowed"
-                                : ""
-                            }`}
-                            onClick={async () => {
-                              if (getPlan === "Basic") return;
-                              try {
-                                setisloading(true);
-                                if (fb.parsedFeedback?.response) {
-                                  await db.handleStatusChange(
-                                    user!,
-                                    "Completed",
-                                    fb.feedback,
-                                    getName
+                <div className="flex justify-center flex-col gap-3 mt-4">
+                  {feedbackData.length > 0 ? (
+                    feedbackData.map((fb, idx) =>
+                      fb?.parsedFeedback?.response ? (
+                        <div
+                          key={idx}
+                          className={`bg-[#2c2016] text-[#d1d1d1] flex items-center rounded-lg p-5 gap-4 border-l-4 border-orange-500 w-full mb-4 ${
+                            getPlan === "Basic"
+                              ? "cursor-not-allowed"
+                              : "cursor-pointer"
+                          }`}
+                        >
+                          <div className="bg-[#244445] p-3 rounded-md text-cyan-400 text-xl">
+                            💡
+                          </div>
+                          <div className="flex-1">
+                            <p
+                              className={`${
+                                getPlan === "Basic" ? "blur-sm" : ""
+                              }`}
+                            >
+                              {fb.parsedFeedback.response.replace(/"/g, "")}
+                            </p>
+                          </div>
+                          <div className="flex items-center space-x-3">
+                            <RxCross2
+                              size={40}
+                              color="red"
+                              className="hover:bg-red-900 p-2 rounded-full cursor-pointer"
+                              onClick={() => {
+                                if (getPlan !== "Basic") {
+                                  setFeedbackData((prev) =>
+                                    prev.filter((_, i) => i !== idx)
                                   );
                                 }
+                              }}
+                            />
+                            <FiCheck
+                              size={40}
+                              color="green"
+                              className={`hover:bg-green-900 p-2 rounded-full cursor-pointer ${
+                                getPlan === "Basic"
+                                  ? "opacity-50 cursor-not-allowed"
+                                  : ""
+                              }`}
+                              onClick={async () => {
+                                if (getPlan === "Basic") return;
+                                try {
+                                  setisloading(true);
+                                  if (fb.parsedFeedback?.response) {
+                                    await db.handleStatusChange(
+                                      user!,
+                                      "Completed",
+                                      fb.feedback,
+                                      getName
+                                    );
+                                  }
 
-                                setFeedbackData((prev) =>
-                                  prev.filter((_, i) => i !== idx)
-                                );
-                              } catch (error) {
-                                console.error("Error updating status:", error);
-                              } finally {
-                                setisloading(false);
-                              }
-                            }}
-                          />
+                                  setFeedbackData((prev) =>
+                                    prev.filter((_, i) => i !== idx)
+                                  );
+                                } catch (error) {
+                                  console.error(
+                                    "Error updating status:",
+                                    error
+                                  );
+                                } finally {
+                                  setisloading(false);
+                                }
+                              }}
+                            />
+                          </div>
                         </div>
+                      ) : null
+                    )
+                  ) : (
+                    <div className="py-10 text-center text-slate-300">
+                      <div className="flex flex-col items-center gap-5">
+                        <FaRegCircleStop size={25} color="#9ca3af" />
+                        <p className="font-semibold">No Analysis Yet</p>
                       </div>
-                    ) : null
-                  )
+                    </div>
+                  )}
+                </div>
+              </div>
+
+              <div className="w-[86vw] md:w-[70vw] lg:w-[36vw] mx-auto p-5 rounded-xl bg-[#161617] h-[50vh] my-5">
+                <div className="flex justify-between">
+                  <div className="flex items-center space-x-3">
+                    <GoGraph size={25} color="#00a3ff" />
+                    <h1 className="font-semibold text-white">User Feedback</h1>
+                  </div>
+                </div>
+
+                {feedbackData.length > 0 ? (
+                  feedbackData.map((fb, idx) => (
+                    <div
+                      key={idx}
+                      className="bg-[#17291f] text-[#d1d1d1] flex items-center rounded-lg p-5 gap-4 border-l-4 border-orange-500 w-full cursor-pointer mt-4"
+                    >
+                      <div className="bg-[#244445] p-3 rounded-md text-cyan-400 text-xl">
+                        💡
+                      </div>
+                      <div className="flex-1">
+                        <p className="text-[#b0b0b0] my-2">{fb.feedback}</p>
+                      </div>
+                    </div>
+                  ))
                 ) : (
-                  <div className="py-10 text-center text-slate-300">
+                  <div className="py-36 text-center text-slate-300">
                     <div className="flex flex-col items-center gap-5">
                       <FaRegCircleStop size={25} color="#9ca3af" />
-                      <p className="font-semibold">No Analysis Yet</p>
+                      <p className="font-semibold">No Feedbacks Yet</p>
                     </div>
                   </div>
                 )}
               </div>
+
+
+
+
+
+
             </div>
-
-            <div className="w-[86vw] md:w-[70vw] lg:w-[58vw] mx-auto p-5 rounded-xl bg-[#161617] h-[50vh] my-5">
-              <div className="flex justify-between">
-                <div className="flex items-center space-x-3">
-                  <GoGraph size={25} color="#00a3ff" />
-                  <h1 className="font-semibold text-white">User Feedback</h1>
-                </div>
+               <div className="flex flex-col lg:flex-row justify-between items-center lg:ml-28 ">
+              <div className="w-[86vw] md:w-[70vw] lg:w-[36vw]  mx-auto  p-5 rounded-xl bg-[#161617] h-[50vh]">
+                {HappyCount > 0 && NeutralCount > 0 && SadCount > 0 ? (
+                  <Doughnut data={analytics} className="w-96 mx-auto p-10" />
+                ) : null}
               </div>
-
-              {feedbackData.length > 0 ? (
-                feedbackData.map((fb, idx) => (
-                  <div
-                    key={idx}
-                    className="bg-[#17291f] text-[#d1d1d1] flex items-center rounded-lg p-5 gap-4 border-l-4 border-orange-500 w-full cursor-pointer mt-4"
-                  >
-                    <div className="bg-[#244445] p-3 rounded-md text-cyan-400 text-xl">
-                      💡
-                    </div>
-                    <div className="flex-1">
-                      <p className="text-[#b0b0b0] my-2">{fb.feedback}</p>
-                    </div>
-                  </div>
-                ))
-              ) : (
-                <div className="py-36 text-center text-slate-300">
-                  <div className="flex flex-col items-center gap-5">
-                    <FaRegCircleStop size={25} color="#9ca3af" />
-                    <p className="font-semibold">No Feedbacks Yet</p>
-                  </div>
-                </div>
-              )}
+              <div className="w-[86vw] md:w-[70vw] lg:w-[36vw]  mx-auto  p-5 rounded-xl bg-[#161617] h-[50vh]">
+                <h1 className="text-gray-300">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum magni laboriosam perspiciatis nesciunt optio nihil cumque quod quam perferendis tempore fugiat libero repellat nam omnis natus fugit, cum animi magnam.
+                </h1>
+              </div>
             </div>
           </div>
         </div>
