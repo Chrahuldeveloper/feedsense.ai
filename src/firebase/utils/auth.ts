46e8cf8d02a917f1275aeb6a3feef856d2357edb
Chrahuldeveloper <chrahulofficial@gmail.com>
2025-08-27 15:12:43 +0530

    FIXED

--- src/firebase/utils/auth.ts
+++ src/firebase/utils/auth.ts
@@ -1,19 +1,9 @@
 "use client";
-// import { doc, getDoc } from "firebase/firestore";
-// import { db } from "../Firebase";
 import { signOut } from "firebase/auth";
 import { auth } from "../Firebase";
 import Cookies from "js-cookie";
 
 class authService {
-  // async checkUser(jwt: any) {
-  //   const userRef = doc(db, "users", jwt);
-  //   const userSnap = await getDoc(userRef);
-  //   if (userSnap.exists()) {
-  //   } else {
-  //     return false;
-  //   }
-  // }
 
   async signOut() {
     try {
